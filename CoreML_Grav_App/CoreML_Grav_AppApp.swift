0672d38d9119b953c41639259c46399f1fb7bf91
Yoshiyuki Kitaguchi <ykitaguchi@gmail.com>
2022-10-23 14:26:34 +0900

    Initial Commit

---
+++ CoreML_Grav_App/CoreML_Grav_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_Grav_AppApp.swift
+//  CoreML_Grav_App
+//
+//  Created by Yoshiyuki Kitaguchi on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_Grav_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
